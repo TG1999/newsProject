88fec17e26c8ebd69abb329be60b687c85205568
tushar goel <tushar.goel.dav@gmail.com>
2018-08-11 22:28:15 +0530

    first
    Signed-off-by: tushar goel <tushar.goel.dav@gmail.com>

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require('express')
+app=express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+const NewsAPI = require('newsapi');
+const {key}=require('./exports')
+const newsapi = new NewsAPI(key);
+const port=process.env.PORT||3000
+app.set('view engine','hbs');
+app.get('/:q',(req,res)=>{
+    newsapi.v2.topHeadlines({
+        q:req.params.q,
+        language:'en'
+      }).then((response)=>{
+          console.log(response)
+          articles=response['articles'];
+          for(i in articles){
+              title=articles[i]['title']
+          des=articles[i]['description']
+          console.log(title)
+          console.log(des)    
+      }
+      res.render('newsfeed',{articles:articles})
+      })      
+})
+app.listen(port,()=>{
+    console.log('http://localhost:3000')
+})
